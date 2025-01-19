bd331c8ff08b68d31d3fe546156fc6ea771e3ce2
lunledev <183235731+lunledev@users.noreply.github.com>
2025-01-18 19:28:45 -0800

    finished nav and added highlighted color for titles

--- component/nav.jsx
+++ component/nav.jsx
@@ -2,7 +2,7 @@ import { Link, useLocation } from 'react-router-dom';
 //Link - navigate to different routes partial page refresh.
 //useLocation - access to current location of object.
 
-function nav() {
+function Navigation() {
     //useLoction() - access to current location object.
     const currentPath = useLocation().pathname; //get current path.
 
@@ -14,15 +14,52 @@ function nav() {
                 <Link to="/"
                     /* 
                     / - root path.
-                    create link for Home, if true then nav-link active else nav-link
+                    create link for developer, if true then nav-link active else nav-link
                     */
                     className={currentPath === '/' ? 'nav-link active' : 'nav-link'}>
-                    Home
-                </Link> 
+                    lunledev
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link to='/About Me'
 
+                    /*
+                    create link for About Me, if true then nav-link active else nav-link
+                    */
+                    className={currentPath === '/About Me' ? 'nav-link active' : 'nav-link'}>
+                    About Me
+                </Link>
+            </li>
 
+            <li className='nav-item'>
+                <Link to='/Portfolio'
 
+                    /*
+                    create link for Portfolio, if true then nav-link active else nav-link
+                    */
+                    className={currentPath === '/Portfolio' ? 'nav-link active' : 'nav-link'}>
+                    Portfolio
+                </Link>
+            </li>
 
+            <li className='nav-item'>
+                <Link to='/Contact'
+                    /*
+                    create link for Contact, if true then nav-link active else nav-link
+                    */
+                    className={currentPath === '/Contact' ? 'nav-link active' : 'nav-link'}>
+                    Contact
+                </Link>
+            </li>
+
+            <li className='nav-item'>
+                <Link to='/Resume'
+                    /*
+                    create link for Resume, if true then nav-link active else nav-link
+                    */
+                    className={currentPath === '/Resume' ? 'nav-link active' : 'nav-link'}>
+                    Resume
+                </Link>
             </li>
 
         </ul>
@@ -36,5 +73,5 @@ function nav() {
 }
 
 
-export default nav;
+export default Navigation;
 
